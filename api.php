e5f431bd4d3be3e0f97ec371c6c5e28172f423c3
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-07-18 21:59:22 -0500

    Enviar datos al servidor 000webhost y respuesta

--- api.php
+++ api.php
@@ -1,6 +1,7 @@
 <?php
     header('Access-Control-Allow-Origin: *');
-
-    echo "Hola mundo soy el mensaje del PHP del servidor : ".$_SERVER["HTTP_HOST"];
-
+    $_DATA = file_get_contents("php://input");
+    $_DATA = json_decode($_DATA, true);
+    extract($_DATA);
+    echo "Hola usuario $nombres $apellidos soy el archivo php recolectado tus datos #$cedula tu numero de identificacion y tu correo electronico es $email, servidor = ".$_SERVER['HTTP_HOST'];
 ?>
