7af4e905dd65092c4a4166f5c5d122fe19a678c0
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-07-22 20:03:10 -0500

    Saber que tipo de peticion realizo "POST, GET"

--- api.php
+++ api.php
@@ -1,7 +1,23 @@
 <?php
     header('Access-Control-Allow-Origin: *');
-    $_DATA = file_get_contents("php://input");
-    $_DATA = json_decode($_DATA, true);
-    extract($_DATA);
+    
+    $cedula = (int) 0;
+    $nombres = (string) "Sistema";
+    $apellidos = (string) "Api";
+    $email = (string) "xxxx@gmail.com";
+
+    $validarDatos = function() use(&$cedula, &$nombres, &$apellidos, &$email):string{
+        $_DATA = file_get_contents("php://input");
+        $_DATA = json_decode($_DATA, true);
+        extract($_DATA);
+        return "EL usuario realizo una peticion POST";
+    };
+    $mensaje = match($_SERVER["REQUEST_METHOD"]){
+        "POST" => $validarDatos(),
+        "GET" => (string) "EL usuario realizo una peticion GET"
+    };
+    echo $mensaje;
+    echo "<br>";
     echo "Hola usuario $nombres $apellidos soy el archivo php recolectado tus datos #$cedula tu numero de identificacion y tu correo electronico es $email, servidor = ".$_SERVER['HTTP_HOST'];
+
 ?>
